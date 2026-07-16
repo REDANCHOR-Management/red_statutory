2300c735674a57703696368f048c1947c2dabc6d
REDANCHOR-Management <info@redanchor.in>
2026-07-16 16:24:11 +0530

    Update compliance_centre.js

--- red_statutory/page/compliance_centre/compliance_centre.js
+++ red_statutory/page/compliance_centre/compliance_centre.js
@@ -1,1 +1,7 @@
-
+frappe.pages['compliance-centre'].on_page_load = function(wrapper) {
+	var page = frappe.ui.make_app_page({
+		parent: wrapper,
+		title: 'Compliance Centre',
+		single_column: true
+	});
+};
