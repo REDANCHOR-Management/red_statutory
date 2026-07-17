894991942e40c869fc98f904eeb3cc133012463d
REDANCHOR-Management <info@redanchor.in>
2026-07-17 17:58:55 +0530

    Update wage_register.js

--- red_statutory/report/wage_register/wage_register.js
+++ red_statutory/report/wage_register/wage_register.js
@@ -1,3 +1,37 @@
 frappe.query_reports["Wage Register"] = {
-	filters: []
+	filters: [
+		{
+			fieldname: "internal_client_code",
+			label: "Internal Client Code",
+			fieldtype: "Link",
+			options: "Customer",
+			reqd: 1
+		},
+		{
+			fieldname: "month",
+			label: "Month",
+			fieldtype: "Select",
+			options: [
+				"1",
+				"2",
+				"3",
+				"4",
+				"5",
+				"6",
+				"7",
+				"8",
+				"9",
+				"10",
+				"11",
+				"12"
+			],
+			reqd: 1
+		},
+		{
+			fieldname: "year",
+			label: "Year",
+			fieldtype: "Int",
+			reqd: 1
+		}
+	]
 };
