db0422f2a9f95d7cc53634581e1f113ad270a691
REDANCHOR-Management <info@redanchor.in>
2026-07-17 17:27:52 +0530

    Update compliance_centre.py

--- red_statutory/page/compliance_centre/compliance_centre.py
+++ red_statutory/page/compliance_centre/compliance_centre.py
@@ -1,3 +1,13 @@
+import frappe
+
+from red_statutory.utils.document_engine import DocumentEngine
+
+
 @frappe.whitelist()
 def get_salary_slips(internal_client_code, month, year):
-    return []
+
+    return DocumentEngine.build_wage_register(
+        internal_client_code=internal_client_code,
+        month=int(month),
+        year=int(year),
+    )
