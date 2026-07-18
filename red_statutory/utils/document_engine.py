296e9eca2d7a26f1bd4e076abd5922ba911a9d27
REDANCHOR-Management <info@redanchor.in>
2026-07-18 16:16:49 +0530

    2c
    2c

--- red_statutory/utils/document_engine.py
+++ red_statutory/utils/document_engine.py
@@ -5,18 +5,6 @@ def build_wage_register(salary_slips):
     """
     Converts Salary Slip Reader output into a generic
     Wage Register structure.
-
-    Input:
-        List returned by salary_slip_reader.get_salary_slips()
-
-    Output:
-        {
-            headers,
-            earning_components,
-            deduction_components,
-            rows,
-            totals
-        }
     """
 
     earning_components = OrderedDict()
@@ -34,17 +22,35 @@ def build_wage_register(salary_slips):
         for component in slip["deductions"]:
             deduction_components[component] = True
 
-    earning_components = list(earning_components.keys())
-    deduction_components = list(deduction_components.keys())
+    earning_component_names = list(earning_components.keys())
+    deduction_component_names = list(deduction_components.keys())
+
+    # Build component objects for HTML
+
+    earning_components = [
+        {
+            "key": component,
+            "label": component
+        }
+        for component in earning_component_names
+    ]
+
+    deduction_components = [
+        {
+            "key": component,
+            "label": component
+        }
+        for component in deduction_component_names
+    ]
 
     rows = []
 
     totals = {
-        "earnings": {c: 0 for c in earning_components},
-        "deductions": {c: 0 for c in deduction_components},
-        "gross_pay": 0,
-        "total_deduction": 0,
-        "net_pay": 0,
+        "earnings": {c: 0 for c in earning_component_names},
+        "deductions": {c: 0 for c in deduction_component_names},
+        "gross_earnings": 0,
+        "gross_deductions": 0,
+        "net_payable": 0,
     }
 
     # -------------------------------------------------------
@@ -55,7 +61,7 @@ def build_wage_register(salary_slips):
 
         row = {
             "sr_no": sr,
-            "employee": slip["employee"],
+            "employee_id": slip["employee"],
             "employee_name": slip["employee_name"],
             "designation": slip["designation"],
             "company": slip["company"],
@@ -66,14 +72,14 @@ def build_wage_register(salary_slips):
             "payment_days": slip["payment_days"],
             "earnings": {},
             "deductions": {},
-            "gross_pay": slip["gross_pay"],
-            "total_deduction": slip["total_deduction"],
-            "net_pay": slip["net_pay"],
+            "gross_earnings": slip["gross_pay"],
+            "gross_deductions": slip["total_deduction"],
+            "net_payable": slip["net_pay"],
         }
 
         # Earnings
 
-        for component in earning_components:
+        for component in earning_component_names:
 
             amount = slip["earnings"].get(component, 0)
 
@@ -83,7 +89,7 @@ def build_wage_register(salary_slips):
 
         # Deductions
 
-        for component in deduction_components:
+        for component in deduction_component_names:
 
             amount = slip["deductions"].get(component, 0)
 
@@ -91,9 +97,9 @@ def build_wage_register(salary_slips):
 
             totals["deductions"][component] += amount
 
-        totals["gross_pay"] += slip["gross_pay"]
-        totals["total_deduction"] += slip["total_deduction"]
-        totals["net_pay"] += slip["net_pay"]
+        totals["gross_earnings"] += slip["gross_pay"]
+        totals["gross_deductions"] += slip["total_deduction"]
+        totals["net_payable"] += slip["net_pay"]
 
         rows.append(row)
 
