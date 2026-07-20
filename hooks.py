298e6e8979b3848e3b7b38fabaa71d9204fbd201
REDANCHOR-Management <info@redanchor.in>
2026-07-20 13:54:35 +0530

    1
    1

--- hooks.py
+++ hooks.py
@@ -1,1 +1,27 @@
-# RED Statutory hooks
+app_name = "red_statutory"
+app_title = "RED Statutory"
+app_publisher = "REDANCHOR"
+app_description = "Statutory Register Generator"
+app_email = ""
+app_license = "MIT"
+
+
+# Pages
+# ------------------
+
+page_js = {
+    "compliance-centre": "public/js/compliance_centre.js"
+}
+
+
+# Assets
+# ------------------
+
+app_include_css = []
+app_include_js = []
+
+
+# Fixtures
+# ------------------
+
+fixtures = []
