af0b07406c83a3f3226178f08e1b58407588b5b6
REDANCHOR-Management <info@redanchor.in>
2026-07-20 13:56:07 +0530

    z
    z

--- page/compliance_centre/compliance_centre.js
+++ page/compliance_centre/compliance_centre.js
@@ -1,1 +1,37 @@
-// TODO: Compliance Centre frontend
+frappe.pages['compliance-centre'].on_page_load = function (wrapper) {
+
+	const page = frappe.ui.make_app_page({
+		parent: wrapper,
+		title: 'Compliance Centre',
+		single_column: true
+	});
+
+	const app = $(`
+		<div class="p-4">
+
+			<div id="filters"></div>
+
+			<div class="mt-4">
+				<button class="btn btn-primary" id="generate_btn">
+					Generate
+				</button>
+			</div>
+
+		</div>
+	`);
+
+	$(page.body).append(app);
+
+	// Filters will be added here.
+
+	$('#generate_btn').on('click', function () {
+
+		// Collect filters
+
+		// Call backend
+
+		// Download PDF
+
+	});
+
+};
--- page/compliance_centre/compliance_centre.py
+++ page/compliance_centre/compliance_centre.py
@@ -1,7 +1,23 @@
-"""
-Compliance Centre backend.
-Responsibilities:
-- Receive filters
-- Call Wage Register PDF generator
-- Return PDF
-"""
+import frappe
+
+
+@frappe.whitelist()
+def generate(register, filters=None):
+    """
+    Compliance Centre launcher.
+
+    Receives:
+        register
+        filters
+
+    Calls the selected register generator.
+
+    Returns:
+        PDF
+    """
+
+    if register == "Wage Register":
+        from red_statutory.registers.wage_register.pdf import generate
+        return generate(filters)
+
+    frappe.throw("Unsupported Register")
